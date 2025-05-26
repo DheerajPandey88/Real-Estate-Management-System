e46513e71cd0a27d66f9156c8acb9915d2cfd7c0
DheerajPandey88 <pandeydhirajkumar001@gmail.com>
2025-05-26 09:31:21 +0530

    Update MAIN_APP_WINDOW.java

--- MAIN_APP_WINDOW.java
+++ MAIN_APP_WINDOW.java
@@ -5,16 +5,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.border.Border;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rkk04
- */
 public class MAIN_APP_WINDOW extends javax.swing.JFrame {
 
     /**
